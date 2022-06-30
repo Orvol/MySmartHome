6029202cdedc17b8bc9fa8766b8b4f5464b1c4aa
Lovfro Šafran <lovro.safran@gmail.com>
2022-06-30 20:05:53 +0200

    added settings tab

--- moj_pokusaj_smart_home/gui.py
+++ moj_pokusaj_smart_home/gui.py
@@ -12,15 +12,18 @@ frame1 = tk.Frame(notebook, width=400, height=280, bg="#3ADF00")
 frame2 = tk.Frame(notebook, width=400, height=280, bg="#B40431")
 frame3 = tk.Frame(notebook, width=400, height=280, bg="#2E64FE")
 frame4 = tk.Frame(notebook, width=400, height=280, bg="#FFFF00")
+frame5 = tk.Frame(notebook, width=400, height=280, bg="#848484")
 
 frame1.pack(fill='both', expand=True)
 frame2.pack(fill='both', expand=True)
 frame3.pack(fill='both', expand=True)
 frame4.pack(fill='both', expand=True)
+frame5.pack(fill='both', expand=True)
 
 notebook.add(frame1, text='TV')
 notebook.add(frame2, text='Grijanje')
 notebook.add(frame3, text='Rasvjeta')
 notebook.add(frame4, text='Ulazna vrata')
+notebook.add(frame5, text='Settings')
 
 root.mainloop()
